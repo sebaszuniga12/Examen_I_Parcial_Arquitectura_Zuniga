21a79f5d837afdc7a76d38e6970695339248c4b2
sebaszuniga12 <sebaszuni12@yahoo.com>
2021-11-29 13:53:18 -0500

    Server

---
+++ Server/src/test/java/ec/edu/espe/arqsoftware/examen/zuniga/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arqsoftware.examen.zuniga.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
